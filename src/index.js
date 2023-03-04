ab9b0c7ff6ab517e9962c1f1b1c70da0922afa0a
samuel ho <samuelho343@gmail.com>
2023-03-03 17:11:47 -0800

    app routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, AppRegistry } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => (
+  <View style={styles.container}>
+    <Text>Open up App.js to start working on your app!</Text>
+    <StatusBar style="auto" />
+  </View>
+);
 
 AppRegistry.registerComponent('App', () => App);
 
