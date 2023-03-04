f5726d891d90346da598cc3de5494b59799f195c
Jake <jakedavisemail@gmail.com>
2023-03-04 16:04:10 -0700

    Boiler plate

---
+++ src/pages/Listing/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles.js';
+
+function ListingInfo({ listingId }) {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+}
+
+export default ListingInfo;
