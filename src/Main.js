e1f0e82737497782cc914eb351b1576265c368d0
Jake <jakedavisemail@gmail.com>
2023-03-04 16:09:23 -0700

    Working Boiler

--- src/Main.js
+++ src/Main.js
@@ -10,6 +10,8 @@ import ChatPage from './pages/Chat';
 import TradesPage from './pages/Trades';
 import UploadPage from './pages/Upload';
 
+import ListingInfo from './pages/ListingInfo/index.js';
+
 const Tab = createBottomTabNavigator();
 
 function Main() {
@@ -17,7 +19,7 @@ function Main() {
     <Navbar
       pages={(
         <>
-          <Tab.Screen name="Home" component={HomePage} />
+          <Tab.Screen name="Home" component={ListingInfo} />
           <Tab.Screen name="Chat" component={ChatPage} />
           <Tab.Screen name="Trades" component={TradesPage} />
           <Tab.Screen name="Upload" component={UploadPage} />
