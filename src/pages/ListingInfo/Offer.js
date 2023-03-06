39fe6d11e599483b7329ae054aa12c9217fe82b7
Jake <jakedavisemail@gmail.com>
2023-03-06 16:40:15 -0700

    Cards Mini faltlist now displays

--- src/pages/ListingInfo/Offer.js
+++ src/pages/ListingInfo/Offer.js
@@ -1,20 +1,43 @@
-import * as React from 'react';
-import { View, Text, Image } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View, Text, Image, FlatList,
+} from 'react-native';
 
 import styles from './styles';
 
-function Offer({ username, user_profile_picture, cards }) {
+function Offer({
+  username, user_profile_picture, rep, cards,
+}) {
   return (
-    <View>
+    <View style={styles.offer}>
       <View style={styles.offerCard}>
         <Image style={styles.smallPfp} source={{ uri: user_profile_picture }} />
         <View style={styles.offerDetails}>
-          <Text style={styles.offerText}>{username}</Text>
+          <View>
+            <Text style={styles.offerText}>
+              {username}
+              {' \n'}
+              {' '}
+              {rep || '0'}
+              {' '}
+              PokeStars
+              {' '}
+            </Text>
+          </View>
         </View>
       </View>
-      <View>
-        {cards.map((card) => <Image style={styles.miniCard} source={{ uri: card.image }} />)}
-      </View>
+      <FlatList
+        style={{
+          left: 80, height: 70, flex: 1, width: 250,
+        }}
+        horizontal
+        data={cards}
+        ListEmptyComponent={<Text>NO DATA</Text>}
+        renderItem={({ item }) => (
+          <Image style={styles.miniCard} source={{ uri: item.image }} />
+        )}
+        keyExtractor={(item, index) => index}
+      />
     </View>
   );
 }
--- src/pages/Trades.js
+++ src/pages/Trades.js
@@ -1,55 +1,58 @@
 import React, { useState } from 'react';
-import { Image, Button, FlatList, ScrollView, StyleSheet, Text, View } from 'react-native';
+import {
+  Image, Button, FlatList, ScrollView, StyleSheet, Text, View,
+} from 'react-native';
 
 import MiniListing from '../components/common/MiniListing.jsx';
 
-import TEST_DATA from '../../dev/test_data/data_trade.js'
+import TEST_DATA from '../../dev/test_data/data_trade.js';
+
 console.log('TEST DATA BEING USED', TEST_DATA[0]);
 
-
-const Trades = ({ navigation }) => {
+function Trades({ navigation }) {
   const [currentView, setCurrentView] = useState(0);
 
   return (
     <View style={{ flex: 1 }}>
       <View>
-        <Button color={currentView === 0 ? 'green' : 'grey'} title='MY ACTIVE LISTINGS' onPress={() => setCurrentView(0)} />
-        <Button color={currentView === 1 ? 'green' : 'grey'} title='MY PLACED OFFERS' onPress={() => setCurrentView(1)} />
+        <Button color={currentView === 0 ? 'green' : 'grey'} title="MY ACTIVE LISTINGS" onPress={() => setCurrentView(0)} />
+        <Button color={currentView === 1 ? 'green' : 'grey'} title="MY PLACED OFFERS" onPress={() => setCurrentView(1)} />
       </View>
       <View style={{ flex: 1, alignItems: 'stretch', justifyContent: 'center' }}>
         {currentView === 0
-          ?
-          <>
-            <Text>
-              INCOMING HEADER
-            </Text>
-            <FlatList
-              style={{ flex: 1 }}
-              data={TEST_DATA}
-              ListEmptyComponent={<Text>NO DATA</Text>}
-              renderItem={({ item }) => <MiniListing listing={item} />} // item is the hardcoded value for FlatList
-              keyExtractor={(listing, index) => listing.id + index}
-            />
-          </>
+          ? (
+            <>
+              <Text>
+                INCOMING HEADER
+              </Text>
+              <FlatList
+                style={{ flex: 1 }}
+                data={TEST_DATA}
+                ListEmptyComponent={<Text>NO DATA</Text>}
+                renderItem={({ item }) => <MiniListing listing={item} />} // item is the hardcoded value for FlatList
+                keyExtractor={(listing, index) => listing.id + index}
+              />
+            </>
+          )
 
-          :
-          <>
-            <Text>
-              OUTGOING HEADER
-            </Text>
+          : (
+            <>
+              <Text>
+                OUTGOING HEADER
+              </Text>
 
-            <FlatList
-              style={{ flex: 1 }}
-              data={[]}
-              ListEmptyComponent={<Text>NO DATA</Text>}
-              renderItem={({ item }) => <Text style={{ paddingTop: 20 }}>{item.title}</Text>}
-              keyExtractor={(item, index) => item.id + index}
-            />
-          </>
-        }
+              <FlatList
+                style={{ flex: 1 }}
+                data={[]}
+                ListEmptyComponent={<Text>NO DATA</Text>}
+                renderItem={({ item }) => <Text style={{ paddingTop: 20 }}>{item.title}</Text>}
+                keyExtractor={(item, index) => item.id + index}
+              />
+            </>
+          )}
       </View>
-    </View >
+    </View>
   );
 }
 
-export default Trades
+export default Trades;
