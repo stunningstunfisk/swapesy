a231d2be85042421931088f6f2708b9352c09641
samuel ho <samuelho343@gmail.com>
2023-03-06 09:23:07 -0800

    modal start

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,17 +1,27 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Pressable } from 'react-native';
+import ModalView from '../components/common/modals/ModalView';
 
 const Upload = ({ navigation }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleModal = () => {
+    setModalVisible(!modalVisible);
+  };
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text
-          onPress={() => navigation.navigate('Home')}
-          style={{ fontSize: 26, fontWeight: 'bold' }}
+        onPress={() => navigation.navigate('Home')}
+        style={{ fontSize: 26, fontWeight: 'bold' }}
       >
         Upload Screen
       </Text>
+      {/* <ModalView handleModal={handleModal} modalVisible={modalVisible} />
+      <Pressable onPress={handleModal}>
+        <Text>Show Modal</Text>
+      </Pressable> */}
     </View>
   );
-}
+};
 
-export default Upload
+export default Upload;
