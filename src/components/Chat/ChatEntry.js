2fbbba0a51fde1520dedf01636abf824bf846a86
Mark <not.markyun@gmail.com>
2023-03-07 15:15:24 -0800

    pesky eslintin

--- src/components/Chat/ChatEntry.js
+++ src/components/Chat/ChatEntry.js
@@ -3,16 +3,16 @@ import React, { useEffect } from 'react';
 import { ListItem, Avatar } from '@rneui/themed';
 
 function ChatEntry({ chat, viewMessages }) {
-  function handlePress() {
+  const handlePress = () => {
     viewMessages(chat.chatId, chat.messageWith.uid);
-  }
+  };
 
-  useEffect(() => { console.log('chatentry', chat.messageWith.uid); }, []);
+  // useEffect(() => { console.log('chatentry', chat.messageWith.uid); }, []);
 
   return (
     <ListItem
       button
-      onPress={() => { handlePress(); }}
+      onPress={handlePress}
       key={chat.chatId}
     >
       <Avatar
