d357ff0fc1b624344378b4246766521473653a80
alibacova <libacova.a@gmail.com>
2023-03-06 22:46:02 -0800

    Fix linter issues

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 
-const Home = ({ navigation }) => {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => alert('This is the Home screen.')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Hello</Text>
-        </View>
-    );
+function Home({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text
+        onPress={() => alert('This is the Home screen.')}
+        style={{ fontSize: 26, fontWeight: 'bold' }}
+      >
+        Hello
+      </Text>
+    </View>
+  );
 }
 
 export default Home;
