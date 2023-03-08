659216e736862fe2684878ac45ae1ad110ba9786
Jake <jakedavisemail@gmail.com>
2023-03-08 13:15:35 -0700

    Sorts and Filters

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,35 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 
-import {
-  getFirestore,
-  collection,
-  getDoc,
-  getDocs,
-  query,
-  doc,
-  where,
-} from 'firebase/firestore';
-
-import firebase from '../../config/firebase';
 import AllListings from '../UserProfile/CurrentListings';
+import listingFuncs from './listingFuncs.js';
 
-const db = getFirestore(firebase);
-
-const getRecentListings = Promise.all(cards.map((cardData) => {
-  const cardId = cardData;
-  const cardRef = doc(db, `card/${cardId}`);
-  const cardQ = query(cardRef);
-  return getDoc(cardQ)
-    .then((da) => da.data())
-    .catch((err) => console.error(err));
-}))
-  .then((x) => setListingCards(x));
+const { getRecentListings, getReputableListings } = listingFuncs;
 
 function Home({ navigation }) {
+  const [listings, setListings] = useState(undefined);
+
+  useEffect(() => { getReputableListings(setListings); }, []);
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <AllListings listings={undefined} />
+      <AllListings listings={listings} />
     </View>
   );
 }
---
+++ src/pages/Home/listingFuncs.js
@@ -0,0 +1,85 @@
+import {
+  getFirestore,
+  collection,
+  getDoc,
+  getDocs,
+  query,
+  doc,
+  where,
+  orderBy,
+} from 'firebase/firestore';
+
+import firebase from '../../config/firebase';
+
+const db = getFirestore(firebase);
+
+const filterFuncs = {
+  price: (listings, price, set) => {
+    set(listings.filter((listing) => listing.type === 'buy' && listing.price <= price));
+  },
+  type: (listings, type, set) => {
+    set(listings.filter((listing) => listing.type === type || listing.type === 'both'));
+  },
+  // filterDistance: (set, r, userLoc) => {
+  //   const ref = collection(db, 'listing');
+  //   const q = query(ref, orderBy('timestamp'), where('completed', '==', false));
+  //   const extracted = [];
+  //   return getDocs(q)
+  //     .then((x) => x.forEach((y) => {
+  //       extracted.push(y.data());
+  //     }))
+  //     .then(() => extracted.filter((listing) => {
+  //       const R = 6371e3; // metres
+  //       const φ1 = (lat1 * Math.PI) / 180; // φ, λ in radians
+  //       const φ2 = (lat2 * Math.PI) / 180;
+  //       const Δφ = ((lat2 - lat1) * Math.PI) / 180;
+  //       const Δλ = ((lon2 - lon1) * Math.PI) / 180;
+
+  //       const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2)
+  //         + Math.cos(φ1) * Math.cos(φ2)
+  //         * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+  //       const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  //       const d = (R * c) / 1609; // in miles
+  //       console.log(d);
+  //       return d < r;
+  //     }))
+  //     .then(() => set(extracted))
+  //     .catch((err) => console.error(err));
+  // },
+};
+
+export default {
+  getRecentListings: (set, filter, filterVal) => {
+    const ref = collection(db, 'listing');
+    const q = query(ref, orderBy('timestamp'), where('completed', '==', false));
+    const extracted = [];
+    return getDocs(q)
+      .then((x) => x.forEach((y) => {
+        extracted.push(y.data());
+      }))
+      .then(() => (filter ? filterFuncs[filter](extracted, filterVal, set) : set(extracted)))
+      .catch((err) => console.error(err));
+  },
+  getReputableListings: (set, filter, filterVal) => {
+    const ref = collection(db, 'listing');
+    const q = query(ref, where('completed', '==', false));
+    let extracted = [];
+    return getDocs(q)
+      .then((x) => x.forEach((y) => {
+        extracted.push(y.data());
+      }))
+      .then(() => Promise.all(extracted.map((listing) => {
+        const userRef = doc(db, `user/${listing.user === 'id' ? 'AshKetchum' : listing.user}`);
+        const userQ = query(userRef);
+        return getDoc(userQ)
+          .then((x) => { listing.reputation = x.data().reputation; return listing; })
+          .catch((err) => console.error(err));
+      })))
+      .then((data) => {
+        extracted = data.sort((a, b) => a.reputation + b.reputation);
+      })
+      .then(() => (filter ? filterFuncs[filter](extracted, filterVal, set) : set(extracted)))
+      .catch((err) => console.error(err));
+  },
+};
