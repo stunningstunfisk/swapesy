9a90a0e56aa45161b0b0484a0c1334ddc953bf35
Mark <not.markyun@gmail.com>
2023-03-08 10:07:18 -0800

    add signup

--- App.js
+++ App.js
@@ -20,9 +20,9 @@ export default function App() {
 
   return (
     <ThemeProvider>
-      {/* {user ? <Main user={user}/> : <AuthStack />} */}
+      {user ? <Main user={user} /> : <AuthStack />}
       {/* TODO: DO NOT RELEASE THIS INTO PRODUCTION */}
-      <Main />
+      {/* <Main /> */}
     </ThemeProvider>
   );
 }
