1b1d107524d033b52588ddd4f4f61604defc6481
samuel ho <samuelho343@gmail.com>
2023-03-09 10:19:46 -0800

    usercard

--- src/components/common/modals/UserCards.jsx
+++ src/components/common/modals/UserCards.jsx
@@ -1,34 +1,32 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
-import cardStyles from '../../../../styles/userProfile/inventoryCard';
+import { View, Text, Image, FlatList } from 'react-native';
 
 function UserCards({ content }) {
-  console.log(content, 'here');
-  return (
-    <View style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr' }}>
-      {content.map((card, i) => (
-        <View key={i}>
-          <Image
-            source={{ uri: card.uri }}
-            style={{
-              width: '100%',
-              height: 'auto',
-              aspectRatio: 1 / 4,
-              padding: 5,
-              resizeMode: 'contain',
-            }}
-          />
-          <Text>{card.name}</Text>
-        </View>
-      ))}
+  const renderItem = ({ item }) => (
+    <View key={item.id} style={{ paddingRight: 10, paddingLeft: 10 }}>
+      <Image
+        source={{ uri: item.uri }}
+        style={{
+          width: '100%',
+          height: 100,
+          aspectRatio: 5 / 7,
+          padding: 5,
+          resizeMode: 'contain',
+        }}
+      />
+      <Text>{item.name}</Text>
     </View>
+  );
+
+  return (
+    <FlatList
+      data={content}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+      numColumns={3}
+      style
+    />
   );
 }
 
 export default UserCards;
-
-// const style = {
-//   container : {
-
-//   }
-// }
