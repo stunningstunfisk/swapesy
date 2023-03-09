bb178628549d46d08c60b7fc071ce3a9811631e1
alibacova <libacova.a@gmail.com>
2023-03-09 13:46:32 -0800

    Set up conditional rendering

--- src/pages/UserProfile/TransactionInfo.js
+++ src/pages/UserProfile/TransactionInfo.js
@@ -19,13 +19,15 @@ function Transaction({ transaction, owner }) {
     setRating(value);
   };
 
+  // for the field with buyer name: I can either add another query when I'm querying for all the transactions to get the name
+  // or we can store buyer name in the buyer field in listings/offers
   return (
     <View style={styles.transactionWrapper}>
       <Text style={styles.transaction}>{type}</Text>
-      <Text style={styles.transactionTitle}>{transaction.title}</Text>
-      <Text style={styles.transactionPrice}>{`$ ${transaction.price}`}</Text>
-      {transaction.buyer && <Text style={styles.transactionCounterparty}>{transaction.buyer}</Text>}
-      {transaction.timestamp && <Text style={styles.transaction}>{transaction.timestamp}</Text>}
+      <Text style={styles.transactionTitle}>{transaction.title || 'Title missing'}</Text>
+      <Text style={styles.transactionPrice}>{`$ ${transaction.price}` || null}</Text>
+      <Text style={styles.transactionCounterparty}>{transaction.buyer || null}</Text>
+      <Text style={styles.transaction}>{transaction.timestamp}</Text>
       <Text
         style={styles.transaction}
         onPress={rateTransaction}
