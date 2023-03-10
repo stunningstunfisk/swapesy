4fe9e90589df80e27920b31587fa5a35d2c88346
alibacova <libacova.a@gmail.com>
2023-03-10 11:21:08 -0800

    Boilerplate

--- src/pages/UserProfile/EditProfile.js
+++ src/pages/UserProfile/EditProfile.js
@@ -1,42 +1,66 @@
 import * as React from 'react';
-import { View, Text, Image, Pressable } from 'react-native';
+import { useState } from 'react';
+import { View, Image, TextInput, FormButton } from 'react-native';
 // import { useNavigation } from '@react-navigation/native';
+import colors from '../../../styles/globalColors';
+import styles from '../../../styles/userProfile/userProfile';
+import {
+  getFirestore,
+  doc,
+  query,
+  setDoc,
+  getDoc,
+} from 'firebase/firestore';
+import firebase from '../../config/firebase';
+const placeholderImg = 'https://avatars.cloudflare.steamstatic.com/52814099e40125301b521935ccca3b5865898777_full.jpg';
 
-function EditProfile({ navigation, user }) {
+function EditProfile({ user }) {
+  const [userData, setUserData] = useState(null);
+
+  const handleUpdate = async() => {
+    db.collection('user')
+    .doc(user.uid)
+    .update({
+      name: userData.name,
+      bio: userData.bio,
+    })
+    .then(() => {
+      console.log('User Updated!');
+      Alert.alert(
+        'Profile Updated!',
+        'Your profile has been updated successfully.'
+      );
+    })
+  }
+
   return (
     <View style={styles.wrapper}>
       <View style={styles.container}>
-        <Image source={profilePic} style={styles.profileImg} />
+        <Image source={{ uri: user.profile_picture || placeholderImg }} style={styles.profileImg} />
         <View style={styles.userInfoContainer}>
           <View style={styles.subContainer}>
-            <Text
-              numberOfLines={2}
-              ellipsizeMode="tail"
-              style={styles.userName}
-            >
-              {user.name ? user.name : 'Nameless Beautiful Unicorn'}
-            </Text>
-            <Pressable
-              onPress={handlePress}
-              style={styles.button}
-            >
-              <Text>{user.uid === owner.uid ? 'Edit' : 'Message'}</Text>
-            </Pressable>
+            <TextInput
+              placeholder={user.name}
+              placeholderTextColor={colors.dark}
+              autoCorrect={false}
+              value={user.name ? userData.name : ''}
+              onChangeText={(txt) => setUserData({ ...userData, name: txt })}
+              style={styles.textInput}
+            />
           </View>
-          <Text style={styles.reputation}>
-            REP:
-            {' '}
-            {user.reputation ? user.reputation : 0}
-          </Text>
-          <Text style={styles.bio}>
-            {owner.bio ? owner.bio : null}
-          </Text>
+          <TextInput
+            multiline
+            numberOfLines={4}
+            placeholder={user.bio}
+            placeholderTextColor={colors.dark}
+            autoCorrect={false}
+            value={user.bio ? userData.bio : ''}
+            onChangeText={(txt) => setUserData({ ...userData, bio: txt })}
+            style={styles.textInput}
+          />
         </View>
       </View>
-      <SegmentSelect
-        buttons={buttons}
-        views={views}
-      />
+      <FormButton buttonTitle="Update" onPress={handleUpdate} />
     </View>
   );
 }
