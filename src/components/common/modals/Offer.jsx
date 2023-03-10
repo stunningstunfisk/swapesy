689b6329d06cc1418fb494d69a8ad9054af22a4d
samuel ho <samuelho343@gmail.com>
2023-03-09 17:34:45 -0800

    offer modal

---
+++ src/components/common/modals/Offer.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { View, FlatList, Text, Button, TextInput } from 'react-native';
+import selectedCardItem from './selectedCardItem';
+
+function Offer({ content }) {
+  const [bidprice, setBidPrice] = useState();
+
+  return (
+    <>
+      <Text>Place your offer</Text>
+
+      <FlatList
+        data={content.cards}
+        renderItem={(item) =>
+          selectedCardItem(
+            item.item,
+            content.handleSelectedCards,
+            content.selectedCards,
+          )
+        }
+        keyExtractor={(item) => item.id}
+        numColumns={3}
+        contentContainerStyle={{
+          margin: 0,
+          padding: 0,
+          flexGrow: 0,
+        }}
+      />
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: 10,
+          alignContent: 'center',
+          padding: 10,
+        }}
+      >
+        <Text>Enter Bid Price:</Text>
+        <TextInput
+          placeholder="enter price"
+          keyboardType="numeric"
+          onChangeText={(e) => setBidPrice(e)}
+          value={bidprice}
+        />
+      </View>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: 10,
+          alignContent: 'center',
+          padding: 10,
+        }}
+      >
+        <Text>Enter Bid Price:</Text>
+        <TextInput
+          placeholder="enter price"
+          keyboardType="numeric"
+          onChangeText={(e) => setBidPrice(e)}
+          value={bidprice}
+        />
+      </View>
+      <Button title="Submit" />
+    </>
+  );
+}
+
+export default Offer;
