789921ff9ab6712e08ed45144ceb77b9f205100e
samuel ho <samuelho343@gmail.com>
2023-03-10 14:10:22 -0800

    styling

--- src/components/common/modals/selectedCardItem.jsx
+++ src/components/common/modals/selectedCardItem.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import { View, TouchableOpacity, Image, Text, Button } from 'react-native';
 
 const selectedCardItem = (item, handleSelectedCards, selectedCards) => (
-  <View key={item.id} style={{ paddingRight: 15, paddingLeft: 15 }}>
+  <View
+    key={item.id}
+    style={{
+      paddingRight: 15,
+      paddingLeft: 15,
+      width: 130,
+      overflow: 'hidden',
+    }}
+  >
     <TouchableOpacity onPress={() => handleSelectedCards.handleClick(item)}>
       <Image
         source={{ uri: item.uri }}
